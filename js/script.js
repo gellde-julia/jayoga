577de1640be98a2dec109b97db9e6f2ee1581819
Gellde <gelldesmodus@gmail.com>
2023-09-25 16:50:29 +0300

    splash window

--- js/script.js
+++ js/script.js
@@ -74,4 +74,22 @@ window.addEventListener('DOMContentLoaded', function () {
         }
 
         setClock('timer', deadline);
+
+        
+        //Модальное окно
+        let more = document.querySelector('.more'),
+            overlay = document.querySelector('.overlay'),
+            close = document.querySelector('.popup-close');
+
+        more.addEventListener('click', function() {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow ='hidden';//запрет прокрутки страницы за открытым модальным окном
+        });
+
+        close.addEventListener('click', function() {
+            overlay.style.display = 'none';
+            more.classList.remove('more-splash');
+            document.body.style.overflow ='';     
+        });
 });
