55161e7bd965d555e20d44e315c4cab978c986e8
Gellde <gelldesmodus@gmail.com>
2023-10-03 19:29:25 +0300

    slider tap

--- js/script.js
+++ js/script.js
@@ -141,4 +141,54 @@ window.addEventListener('DOMContentLoaded', function () {
             }
 
         });
+
+        
+        //слайдер
+        let slideIndex = 1, //первый слайд
+            slides = document.querySelectorAll('.slider-item'), //каждый слайд
+            prev = document.querySelector('.prev'), //навигация назад
+            next = document.querySelector('.next'), //навигация вперед
+            dotsWrap = document.querySelector('.slider-dots'), //обертка точек
+            dots = document.querySelectorAll('.dot'); //каждая точка
+
+            showSlides(slideIndex);
+
+            function showSlides(n) {
+                
+                if (n > slides.length) {
+                    slideIndex = 1;
+                }
+                if (n < 1) {
+                    slideIndex = slides.length;
+                }
+                
+                slides.forEach((item) => item.style.display = 'none');
+                dots.forEach((item) => item.classList.remove('dot-active'));
+
+                slides[slideIndex - 1].style.display = 'block';
+                dots[slideIndex - 1].classList.add('dot-active');
+            }
+
+            function plusSlides(n) {
+                showSlides(slideIndex += n);
+            }
+            function currentSlide(n) {
+                showSlides(slideIndex = n);
+            }
+
+            prev.addEventListener('click', function(){
+                plusSlides(-1);
+            });
+
+            next.addEventListener('click', function(){
+                plusSlides(1);
+            });
+
+            dotsWrap.addEventListener('click', function(event) {
+                for (let i = 0; i < dots.length + 1; i++) {
+                    if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+                        currentSlide(i);
+                    }
+                }
+            });
 });
